962216df30dcb42d46c5768edc342eaab0376184
Zuzanna Szymańska <136966490+Konume@users.noreply.github.com>
2025-04-15 19:23:58 +0200

    cleaning

--- server/test.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-const password = 'qaz123'; // Hasło z formularza
-const hashedPassword = '$2a$10$cMTdLA2f93qfyhutrNXQM.0s5r0lE/v9tWt8QQpXX4cwwSpRPle82'; // Hasło zapisane w bazie
-
-bcrypt.compare(password, hashedPassword).then(isMatch => {
-  console.log(isMatch); // Powinno zwrócić true, jeśli hasła są zgodne
-}).catch(err => console.error(err));
